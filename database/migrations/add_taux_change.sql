9a0cddc68c3c9def6c6b502720e7fbf868b0ec18
james-mathe <kambalemathejacques3o@gmail.com>
2026-05-04 16:36:14 +0200

    removing the not important folder and files

--- database/migrations/add_taux_change.sql
+++
@@ -1,4 +0,0 @@
--- Ajouter le paramètre taux_change pour la conversion USD/CDF
-INSERT INTO `parametres` (`cle`, `valeur`, `type`) 
-VALUES ('taux_change', '2800', 'number')
-ON DUPLICATE KEY UPDATE `valeur` = '2800';
