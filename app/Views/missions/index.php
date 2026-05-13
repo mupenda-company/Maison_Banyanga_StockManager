077659d3299253456b1db279763202d154f09f44
james-mathe <kambalemathejacques3o@gmail.com>
2026-05-13 15:43:41 +0200

    update the edit button

--- app/Views/missions/index.php
+++ app/Views/missions/index.php
@@ -144,7 +144,7 @@ ob_start();
                                 <?php if ($mission['statut'] === 'en_cours'): ?>
                                 <a href="<?= url('missions/' . $mission['id'] . '/edit') ?>" class="text-blue-600 hover:text-blue-700" title="Modifier">
                                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5h2M12 5v14m7-7H5"/>
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
                                     </svg>
                                 </a>
                                 <?php endif; ?>
--- app/Views/missions/show.php
+++ app/Views/missions/show.php
@@ -26,7 +26,7 @@ ob_start();
                     <?php if ($mission['statut'] === 'en_cours'): ?>
                     <a href="<?= url('missions/' . $mission['id'] . '/edit') ?>" class="btn btn-sm btn-secondary">
                         <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5h2M12 5v14m7-7H5"/>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
                         </svg>
                         Modifier
                     </a>
